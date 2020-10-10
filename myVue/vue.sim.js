cb1c13d1070f6210c7befb9f189cab07f10d5315
dkg <604144362@qq.com>
2020-10-10 18:09:01 +0800

    update

--- myVue/vue.sim.js
+++ myVue/vue.sim.js
@@ -431,7 +431,17 @@ function initAssetRegisters (Vue) {
             if (!definition) {
                 return this.options[type + 's'][id]
             } else {
-                
+                if (type === 'component' && isPlainObject(definition)) {
+                    definition.name = definition.name || id
+                    definition = this.options._base.extend(definition)
+                }
+
+                if (type === 'directive' && typeof definition === 'function') {
+                    definition = {bind: definition, update: definition}
+                }
+
+                this.options[type + 's'][id] = definition
+                return definition
             }
         }
     })
